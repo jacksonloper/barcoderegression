98dc4a4a884f9b05c96ab98d41513ca877c7d36b
Jackson Loper <jloper@Jacksons-MacBook-Pro.local>
2020-04-20 11:30:21 -0400

    maybe manifest.in will help me with package data

--- setup.py
+++ setup.py
@@ -4,9 +4,10 @@ setup(
     name='barcoderegression',
     author='Jackson Loper',
     version='0.0.1',
+    include_package_data=True,
     description='Tools for deconvolution and demixing',
     packages=['barcoderegression'],
     package_data={
-        'barcoderegression': ['*.pkl']
+        '': ['*.pkl']
     }
 )
